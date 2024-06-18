1ec1e9e69bd60cfc1bb7ea092985adc0fbc8f74d
AbdelmalekIla <115870374+AbdelmalekIla@users.noreply.github.com>
2024-06-18 19:50:44 +0100

    Implemented full board check to end game in tic-tac-toe

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-
-
-
-
-
 def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 9)
 
+def is_board_full(board):
+    for row in board:
+        if " " in row:
+            return False
+    return True
+
 
 def initialize_board():
-    # Create an empty list to hold the rows of the board
     board = []
-
-    # Loop 3 times to create each row
     for i in range(3):
-        # Create a row list with 3 empty strings
         row = []
         for j in range(3):
             row.append(" ")
-        # Add the row to the board
         board.append(row)
-
-    # Print the resulting board
     return board
+
 
 def make_move(board ,slot ,player,):
     row = (slot - 1) // 3
@@ -41,11 +36,15 @@ def main():
     board = initialize_board()
     print_board(board)
 
-    slot_1 = int(input("Player 1, you have 'X'. Please enter a slot number from 1 to 9 to place your 'X': "))
-    make_move(board, slot_1, "X")
-    print_board(board)
-    slot_2 = int(input("Player 2, you have 'O'. Please enter a slot number from 1 to 9 to place your 'O': "))
-    make_move(board, slot_2, "O")
-    print_board(board)
+    while not is_board_full(board):
+        slot_1 = int(input("Player 1, you have 'X'. Please enter a slot number from 1 to 9 to place your 'X': "))
+        make_move(board, slot_1, "X")
+        print_board(board)
+
+        if is_board_full(board) :
+            break
+        slot_2 = int(input("Player 2, you have 'O'. Please enter a slot number from 1 to 9 to place your 'O': "))
+        make_move(board, slot_2, "O")
+        print_board(board)
 
 print(main())
